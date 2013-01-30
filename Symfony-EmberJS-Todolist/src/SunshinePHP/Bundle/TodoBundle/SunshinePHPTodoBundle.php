e74468dc158a3ddd53d0e756accabafcca1fa2ab
Dustin Whittle <dustin.whittle@gmail.com>
2013-01-30 03:22:51 -0800

    refactored example app on symfony 2.2 + added todo bundle stub

---
+++ Symfony-EmberJS-Todolist/src/SunshinePHP/Bundle/TodoBundle/SunshinePHPTodoBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SunshinePHP\Bundle\TodoBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SunshinePHPTodoBundle extends Bundle
+{
+}
--- Symfony-EmberJS-Todolist/web/app.php
+++ Symfony-EmberJS-Todolist/web/app.php
@@ -3,21 +3,16 @@
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-
-// Use APC for autoloading to improve performance
-// Change 'sf2' by the prefix you want in order to prevent key conflict with another application
-/*
-$loader = new ApcClassLoader('sf2', $loader);
+$loader = require_once __DIR__.'/../app/autoload.php';
+$loader = new ApcClassLoader('sunshine', $loader);
 $loader->register(true);
-*/
 
 require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
+# require_once __DIR__.'/../app/AppCache.php';
 
-$kernel = new AppKernel('prod', false);
+$kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
+# $kernel = new AppCache($kernel);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
